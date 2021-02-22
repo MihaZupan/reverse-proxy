25615db0d0c5466967e9dd77e71870f4b798cdf5
MihaZupan <mihazupan.zupan1@gmail.com>
2021-02-22 11:45:10 +0100

    Temp

--- samples/BenchmarkApp/Program.cs
+++ samples/BenchmarkApp/Program.cs
@@ -1,8 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -11,18 +14,34 @@ namespace BenchmarkApp
     public class Program
     {
         public static void Main(string[] args) =>
-            CreateHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                    webBuilder.ConfigureKestrel((context, kestrelOptions) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var hostBuilder = WebHost.CreateDefaultBuilder(args)
+                .ConfigureKestrel((context, kestrelOptions) =>
+                {
+                    kestrelOptions.ConfigureHttpsDefaults(httpsOptions =>
                     {
-                        kestrelOptions.ConfigureHttpsDefaults(httpsOptions =>
-                        {
-                            httpsOptions.ServerCertificate = new X509Certificate2(Path.Combine(context.HostingEnvironment.ContentRootPath, "testCert.pfx"), "testPassword");
-                        });
-                    })
-                   .UseStartup<Startup>());
+                        httpsOptions.ServerCertificate = new X509Certificate2(Path.Combine(context.HostingEnvironment.ContentRootPath, "testCert.pfx"), "testPassword");
+                    });
+                })
+                .UseStartup<Startup>();
+
+#if NET
+            hostBuilder.UseSockets(options =>
+            {
+                options.WaitForDataBeforeAllocatingBuffer = false;
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    options.UnsafePreferInlineScheduling = Environment.GetEnvironmentVariable("DOTNET_SYSTEM_NET_SOCKETS_INLINE_COMPLETIONS") == "1";
+                    Console.WriteLine($"{nameof(options.UnsafePreferInlineScheduling)}: {options.UnsafePreferInlineScheduling}");
+                }
+            });
+#endif
+
+            return hostBuilder;
+        }
     }
 }
